748569d9619816660c943fd3e57b34b223da94f6
IIMrFreemanII <nikolay.diahovets@gmail.com>
2022-02-07 23:33:04 +0200

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,23 @@
+import React, { memo } from "react";
+
+import { GameLoop } from "./engine";
+import { Editor } from "./frontent/pages";
+import { useDidMount } from "./frontent/hooks";
+
+import styles from "frontent/assets/styles/app.module.css";
+import wait from "fork-ts-checker-webpack-plugin/lib/utils/async/wait";
+
+export const App = memo(() => {
+  useDidMount(async () => {
+    await wait(0);
+
+    GameLoop.init();
+    GameLoop.start();
+  });
+
+  return (
+    <div className={styles.app}>
+      <Editor />
+    </div>
+  );
+});
